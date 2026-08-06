f57e281c67aaede4512546b6d4db3e615eccd8e1
Xavier051206 <Xavierluisw@gmail.com>
2026-08-06 18:42:55 -0400

    Actualizar conexion a postgres

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 // Esto es lo vital: exportar el pool directamente
 module.exports = pool;
+
 // Mensaje para confirmar que nos conectamos bien
 pool.connect((err) => {
     if (err) {
@@ -19,5 +17,3 @@ pool.connect((err) => {
         console.log('✅ Base de Datos conectada con éxito');
     }
 });
-
-module.exports = pool;
